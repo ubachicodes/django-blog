0cafff6daff95d8e2902702f0b711285040d84ae
ubachicodes . <prettyestmail@gmail.com>
2024-03-03 19:10:40 +0000

    Add About app

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,26 @@ from .models import Post
 
 # Create your views here.
 
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(Event, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
